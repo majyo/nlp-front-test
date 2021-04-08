d936941945287be564efb17b100108554c72cf0c
majyoarisu <majyoarisu@outlook.com>
2021-04-09 00:06:17 +0800

    fix CORS

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,14 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 8002
+    port: 8011
+  },
+  proxy: {
+    '/api': {
+      target: 'http://192.168.40.7:8009',
+      changeOrigin: true,
+      rewrite: path => path.replace(/^\/api/, '')
+    }
   },
   plugins: [vue()]
 })
