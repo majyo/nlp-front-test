c6f61d74099858d094fb2d7188c8c00b3ec28e0f
majyoarisu <majyoarisu@outlook.com>
2021-04-30 02:18:35 +0800

    add oie

---
+++ src/components/Dygie.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="left-align">
+    <h2 class="left-align">GPT语言模型</h2>
+    <el-divider></el-divider>
+    <p class="norm-margin">
+      使用dygie++模型搭建的联合式关系抽取任务。
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+name: "Dygie"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Oie.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="left-align">
+    <h2 class="left-align">开放信息抽取</h2>
+    <el-divider></el-divider>
+    <p class="norm-margin">
+      使用AllenNLP提供的任务模型搭建的开放信息抽取任务。
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+name: "Oie"
+}
+</script>
+
+<style scoped>
+#task {
+  margin-left: 40px;
+  margin-right: 140px;
+}
+.left-align {
+  text-align: left;
+}
+.norm-margin {
+  margin-top: 30px;
+  margin-bottom: 30px;
+}
+.small-margin {
+  margin-top: 15px;
+  margin-bottom: 15px;
+}
+</style>
